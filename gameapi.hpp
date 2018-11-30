16b40bcd0f7fd651273f70e52a9a21456988aeb7
James <Icedshot@gmail.com>
2018-11-30 07:25:40 +0000

    fun with gameapi serialisation

--- gameapi.hpp
+++ gameapi.hpp
@@ -6,15 +6,48 @@
 #include "json.hpp"
 #include <memory>
 #include <functional>
+#include <vector>
 
-struct gametype
+struct object
 {
-    std::variant<nlohmann::json, std::function<std::shared_ptr<gametype>()>, std::shared_ptr<gametype>> type;
+    ///maps a key to a type
+    ///so there are fundamentally 3 datatypes here, one is a raw value store in the nlohmann::json
+    ///this will contain actual datatypes
+    ///the second is a function pointer that returns a datatype, this should be used for interop with other languages
+    ///the third is a game type type so we can store recursively and model objects
+    ///maybe instead of the function pointer we need to model js style getter/setters/apply
+    std::map<std::string, std::variant<nlohmann::json, std::function<std::shared_ptr<object>()>, std::shared_ptr<object>>> type;
 };
 
+struct serialisable
+{
+    virtual void handle_serialise(object& o, bool ser){}
+};
+
+///ok so
+///gameapi will be the object we interact with from c++
+///we want to be able to serialise objects to this easily, WHOLE objects as well
+///so the c++ friend injection hack might become viable here
 struct gameapi
 {
 
 };
+
+template<typename T, typename = std::enable_if_t<!std::is_base_of_v<serialisable, T> && std::is_arithmetic_v<std::remove_reference_t<T>>>>
+inline
+void serialise(object& obj, const T& t, const std::string& key, bool ser)
+{
+    obj.type[key] = nlohmann::json(t);
+}
+
+inline
+void serialise(object& obj, serialisable& v, const std::string& key, bool ser)
+{
+    object no;
+
+    v.handle_serialise(no, ser);
+
+    obj.type[key] = std::make_shared<object>(no);
+}
 
 #endif // GAMEAPI_H
