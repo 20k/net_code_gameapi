d40f25e142f341a88f4e4a9af7c928a4c01d8a18
James <Icedshot@gmail.com>
2018-12-03 00:56:38 +0000

    testing gameapi

--- gameapi.hpp
+++ gameapi.hpp
@@ -8,6 +8,16 @@
 #include <functional>
 #include <vector>
 
+///nlohmann not a good choice for this as its very heavily exception based
+
+///this is a simple nlohmann replacement
+///hmm actually we dont need a nlohmann replacement
+///what I just really need is a way to iterate through every member of a struct, and call a host function on it
+/*struct datatype
+{
+
+};
+
 struct object
 {
     ///maps a key to a type
@@ -44,18 +54,22 @@ void for_each_recursive(object& o, const T& func)
             for_each_recursive(*std::get<std::shared_ptr<object>>(i.second), func);
         }
     }
-}
+}*/
 
-#define SERIALISE_FUNC() virtual void handle_serialise(object& o, bool ser)
+using game_api_t = uint32_t;
+
+#define SERIALISE_FUNC() virtual void handle_serialise(game_api_t& o, bool ser)
 
 struct serialisable
 {
-    virtual void handle_serialise(object& o, bool ser){}
+    virtual void handle_serialise(game_api_t& o, bool ser){}
 };
 
-#define SER(obj) serialise(o, obj, #obj, ser);
+//#define SER(obj) serialise(o, obj, #obj, ser);
 
-template<typename T, typename = std::enable_if_t<!std::is_base_of_v<serialisable, T>>>
+#define SER(obj) to_gameapi(o, obj, #obj, ser)
+
+/*template<typename T, typename = std::enable_if_t<!std::is_base_of_v<serialisable, T>>>
 inline
 void serialise(object& obj, T& t, const std::string& key, bool ser)
 {
@@ -86,6 +100,55 @@ void serialise(object& obj, serialisable& v, const std::string& key, bool ser)
     {
         v.handle_serialise(obj, ser);
     }
+}*/
+
+struct c_str
+{
+    c_str(const std::string& str)
+    {
+        len = str.size();
+        ptr = str.c_str();
+    }
+
+    uint32_t len;
+    const char* ptr;
+};
+
+extern void serialise_basic(game_api_t gapi, uint32_t* u, c_str* key, bool ser);
+extern void serialise_basic(game_api_t gapi, uint64_t* u, c_str* key, bool ser);
+extern void serialise_basic(game_api_t gapi, float* u, c_str* key, bool ser);
+extern void serialise_basic(game_api_t gapi, double* u, c_str* key, bool ser);
+extern void serialise_basic(game_api_t gapi, c_str* u, c_str* key, bool ser);
+
+extern void serialise_object_begin(game_api_t gapi, c_str* key);
+extern void serialise_object_end(game_api_t gapi, c_str* key);
+
+//template<typename T, typename = std::enable_if_t<std::is_base_of_v<serialisable, T>>>
+void to_gameapi(game_api_t gapi, serialisable& s, const std::string& key, bool ser)
+{
+    c_str ckey(key);
+
+    serialise_object_begin(gapi, &ckey);
+
+    s.handle_serialise(gapi, ser);
+
+    serialise_object_end(gapi, &ckey);
+}
+
+void to_gameapi(game_api_t gapi, std::string& str, const std::string& key, bool ser)
+{
+    c_str cstr(str);
+    c_str ckey(key);
+
+    serialise_basic(gapi, &cstr, &ckey, ser);
+}
+
+template<typename T, typename = std::enable_if_t<!std::is_base_of_v<serialisable, T>>>
+void to_gameapi(game_api_t& gapi, T& s, const std::string& key, bool ser)
+{
+    c_str ckey(key);
+
+    serialise_basic(gapi, &s, &ckey, ser);
 }
 
 #endif // GAMEAPI_H
