3b1c76bf9e91181df1616e0338505e9a6518e3b8
James <Icedshot@gmail.com>
2018-12-03 13:33:25 +0000

    delete some old code

--- gameapi.hpp
+++ gameapi.hpp
@@ -13,21 +13,6 @@
 ///this is a simple nlohmann replacement
 ///hmm actually we dont need a nlohmann replacement
 ///what I just really need is a way to iterate through every member of a struct, and call a host function on it
-/*struct datatype
-{
-
-};
-
-struct object
-{
-    ///maps a key to a type
-    ///so there are fundamentally 3 datatypes here, one is a raw value store in the nlohmann::json
-    ///this will contain actual datatypes
-    ///the second is a function pointer that returns a datatype, this should be used for interop with other languages
-    ///the third is a game type type so we can store recursively and model objects
-    ///maybe instead of the function pointer we need to model js style getter/setters/apply
-    std::map<std::string, std::variant<nlohmann::json, std::function<std::shared_ptr<object>()>, std::shared_ptr<object>>> type;
-};
 
 ///so what we need is a method to iterate through an object, and be provided with the corresponding concrete type
 
@@ -65,42 +50,7 @@ struct serialisable
     virtual void handle_serialise(game_api_t& o, bool ser){}
 };
 
-//#define SER(obj) serialise(o, obj, #obj, ser);
-
 #define SER(obj) to_gameapi(o, obj, #obj, ser)
-
-/*template<typename T, typename = std::enable_if_t<!std::is_base_of_v<serialisable, T>>>
-inline
-void serialise(object& obj, T& t, const std::string& key, bool ser)
-{
-    if(ser)
-        obj.type[key] = nlohmann::json(t);
-    else
-    {
-        //if(!std::holds_alternative<nlohmann::json>(obj.type[key]))
-        //    throw std::runtime_error("bad key type");
-
-        t = (T)std::get<nlohmann::json>(obj.type[key]);
-    }
-}
-
-//template<typename T, typename = std::enable_if_t<std::is_base_of_v<serialisable, T>>>
-inline
-void serialise(object& obj, serialisable& v, const std::string& key, bool ser)
-{
-    if(ser)
-    {
-        object no;
-
-        v.handle_serialise(no, ser);
-
-        obj.type[key] = std::make_shared<object>(no);
-    }
-    else
-    {
-        v.handle_serialise(obj, ser);
-    }
-}*/
 
 struct c_str
 {
@@ -126,6 +76,8 @@ extern "C" void serialise_object_end(game_api_t gapi, const char* key, bool ser)
 
 extern "C" void serialise_object_begin_base(game_api_t gapi, bool ser);
 extern "C" void serialise_object_end_base(game_api_t gapi, bool ser);
+
+extern "C" game_api_t get_next_id();
 
 //template<typename T, typename = std::enable_if_t<std::is_base_of_v<serialisable, T>>>
 void to_gameapi(game_api_t gapi, serialisable& s, const std::string& key, bool ser)
